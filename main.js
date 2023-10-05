ccd2af41198f4ce8497d1cd4daa6dcd8f37aef08
Mouly <abiraazmary22@gmail.com>
2023-10-05 20:47:04 +0600

    json

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let name = 'Umme Abira Azmary (Mouly)'
 let age = 23
-const salary = 55000
+const birthday = 22012000
 
 console.log(`my name is ${name} and age is ${age}`)
 console.log(name.length)
-console.log(salary.length)
 console.log(name.substring(0,7).toUpperCase())
 console.log(name.split(''))
 console.log(name.split(' '))
@@ -25,3 +24,40 @@ console.log(movie)
 console.log(Array.isArray(movie))
 console.log(Array.isArray(name)) //cause name is not an array
 console.log(movie.indexOf('Princess Mononoke'))
+
+//object literals
+const store = {
+    eggs : 2,
+    vegetables: ['onion', 'papaya', 'chili', 'ginger'],
+    dairy:{
+        milk: 2,
+        cheese: 'Cube'
+    }
+}
+console.log(store)
+console.log(store.vegetables[1])
+console.log(store.dairy.cheese)
+
+const todos=[
+    {
+        id: 1,
+        name: 'mandina',
+        profession: 'teacher'
+    },
+    {
+        id: 2,
+        name: 'Thenos',
+        profession: 'Genetor'
+
+    }
+]
+console.log(todos[1].id)
+
+//for sending a data into a server, we need to convert it into the json format. we can convert a js format into a JSON format following below procedures:
+const todoJSON = JSON.stringify(todos)
+console.log(todoJSON)
+
+//for loop
+for (let i=0; i<5; i++){
+    console.log(i)
+}
