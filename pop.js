dfb7105109b021c38cacf1aca3c8c484c6b09714
Mouly <abiraazmary22@gmail.com>
2023-10-06 22:00:02 +0600

    ..

---
+++ pop.js
@@ -0,0 +1,15 @@
+//ES6 fat arrow function
+// function number(){
+//     return 10
+// }
+//console.log(number())
+//now converting this function into an arrow function. arrow function e ekta name e assign korbo.
+
+const name1 = () => 20 //simplest form of an arrow function
+console.log(name1())
+
+const name2 = () => console.log(30)
+console.log(name2())
+// const name3 = (n) => console.log(30)
+// console.log(name3(30))
+
